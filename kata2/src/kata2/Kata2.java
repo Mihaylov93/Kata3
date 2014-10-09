79491dab406b3075ae41a12ecf0ad78700020c4e
Petrakis <Petrakis@10.230.168.96>
2014-10-09 11:45:07 +0100

    Kata2 Clases

--- kata2/src/kata2/Kata2.java
+++ kata2/src/kata2/Kata2.java
@@ -1,9 +1,27 @@
 package kata2;
 
+import java.util.HashMap;
+
 public class Kata2 {
 
     public static void main(String[] args) {
-
+        int[] vec = {2,2,11,4,2,6,11,2,3,5,11,5};
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < vec.length; i++) {
+            Integer frec = map.get(vec[i]);
+            System.out.println(map.get(vec[i]));
+            if(frec != null) {
+                map.put(vec[i], frec+1 );
+            }else{
+                map.put(vec[i], 1);
+            }
+            
+            for (Integer key : vec) {
+                
+            }
+        }
     }
+    
     
 }
