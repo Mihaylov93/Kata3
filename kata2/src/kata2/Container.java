6a087aacc174b9aa5f6f5e0c9789b1e93e1f86ad
Petrakis <Petrakis@10.230.168.96>
2014-10-09 10:47:23 +0100

    Kata2 Clases

---
+++ kata2/src/kata2/Container.java
@@ -0,0 +1,15 @@
+package kata2;
+
+public class Container {
+    
+    private final int[] vector;
+
+    public Container(int[] vector) {
+        this.vector = vector;
+    }
+
+    public int[] getVector() {
+        return vector;
+    }
+
+}
