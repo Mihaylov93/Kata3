5f154ae3ab1a5308ad4e0a1b3d305efa238309e8
Petrakis <Petrakis@10.230.168.124>
2014-10-23 10:35:30 +0100

    Kata3

---
+++ Kata3/src/kata3/HistogramBuilder.java
@@ -0,0 +1,14 @@
+package kata3;
+
+public class HistogramBuilder <Type> {
+    
+    public Histogram<Type> build (Type[] types){
+        Histogram<Type> result = new Histogram<>();
+        for (Type type : types) {
+            if (result.containsKey(type)) 
+                result.put(type, result.get(type)+1);
+        }
+        return result;
+    
+    }
+}
